a304663a300bbf7e841bb8dfa84b10fb6aaeea29
koushik <koushik.dey@live.com>
2022-07-02 02:43:09 +0300

    mongo atlas connected

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("DBConnection Successful!"))
+    .catch((err) => {
+        console.log(err);
+    });
 
 
-app.listen(8000, () => {
-    console.log("Backend server is running")
-})
+app.listen(process.env.PORT || 8000, () => {
+    console.log("Backend server is on fire!");
+});
