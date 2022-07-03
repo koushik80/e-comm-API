bd9ba44567d2be7c2102fa8a5652a530defd9220
koushik <koushik.dey@live.com>
2022-07-03 22:22:15 +0300

    rty

--- routes/auth.js
+++ routes/auth.js
@@ -58,8 +58,7 @@ router.post('/login', async (req, res) => {
 
         const { password, ...others } = user._doc;
         res.status(200).json({...others, accessToken});
-
-            res.status(200).json(user)
+    
     } catch (err) {
         res.status(500).json(err);
     }
