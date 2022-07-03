4d128625cc33a0423cbb6834d09e69df2ff9aae4
koushik <koushik.dey@live.com>
2022-07-03 16:38:47 +0300

    reg-auth

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,16 @@ const router = require("express").Router();
 const User = require("../models/User");
 
 //REGISTER
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
         password: req.body.password,
     });
+
+    const savedUser = await newUser.save()
+
+
 });
 
 
