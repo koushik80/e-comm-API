349c8be0e16878bfad557029bcf0c4c10f6e5c53
koushik <koushik.dey@live.com>
2022-07-03 17:47:27 +0300

    lgn-auth

--- routes/auth.js
+++ routes/auth.js
@@ -23,5 +23,22 @@ router.post("/register", async (req, res) => {
 
 });
 
+//LOGIN
+
+router.post('/login', async (req, res) => {
+    try{
+        const user = await User.findOne(
+            {
+                userName: req.body.user_name
+            }
+        );
+
+
+
+
+
+
+
+
 
 module.exports = router;
