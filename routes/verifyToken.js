b6f30567989277e33630ac7ace5004f27f4d9d0d
koushik <koushik.dey@live.com>
2022-07-03 23:07:09 +0300

    prc

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers.token;
     if (authHeader) {
-
+        jwt.verify(token, process.env.JWT_SEC)
     } else {
         return res.status(401).json("You are not authenticated!");
     }
