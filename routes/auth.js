1430c9e85fc6694cf9b7483689da3c2fc7d401ab
koushik <koushik.dey@live.com>
2022-07-03 14:05:01 +0300

    usr

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
+//REGISTER
+router.post("/register", (req, res) => {
+    const newUser = new User
+})
 
 
 module.exports = router;
