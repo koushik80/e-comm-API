0b095d581bcc2607cd67d19ad001ac8fc2f58bc5
koushik <koushik.dey@live.com>
2022-07-04 22:56:43 +0300

    dlt

--- routes/user.js
+++ routes/user.js
@@ -26,7 +26,21 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-    
 });
+
+//Delete
+
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+
 
 module.exports = router;
