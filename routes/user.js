894b63703cac59e079585653acb71f6cfe59e98d
koushik <koushik.dey@live.com>
2022-07-04 22:19:34 +0300

    sdf

--- routes/user.js
+++ routes/user.js
@@ -15,6 +15,19 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
         ).toString();
         
     }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+    
 });
 
 module.exports = router;
