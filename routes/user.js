f8cff5b2f4b8298dd576cfdf6ad7bc7823540ee2
koushik <koushik.dey@live.com>
2022-07-04 23:20:03 +0300

    gt-usr

--- routes/user.js
+++ routes/user.js
@@ -50,6 +50,18 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+//GET ALL USER
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 
