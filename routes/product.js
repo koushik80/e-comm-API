78106595a7219675f39cd26d19704fba48035b7d
koushik <koushik.dey@live.com>
2022-07-05 21:29:53 +0300

    prd 5

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,7 @@
 const Product = require("../models/Product");
 const {
-  verifyTokenAndAdmin,
+    verifyToken,
+    verifyTokenAndAdmin
 } = require("./verifyToken");
 
 const router = require("express").Router();
@@ -8,14 +9,14 @@ const router = require("express").Router();
 //CREATE
 
 router.post("/", verifyTokenAndAdmin, async (req, res) => {
-    const newProduct = new Product(req.body);
+  const newProduct = new Product(req.body);
 
-    try {
-        const savedProduct = await newProduct.save();
-        res.status(200).json(savedProduct);
-    }   catch (err) {
-        res.status(500).json(err);
-    }
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 
