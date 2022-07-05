32531746c585e3acb2bd4772b358e6c6e1075eae
koushik <koushik.dey@live.com>
2022-07-05 21:54:02 +0300

    prd-get

--- routes/product.js
+++ routes/product.js
@@ -38,6 +38,7 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
 });
 
 //DELETE
+
 router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
     await Product.findByIdAndDelete(req.params.id);
@@ -47,7 +48,16 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+//GET PRODUCT
 
+router.get("/find/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 
