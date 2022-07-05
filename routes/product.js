bbd52cbd391fdce3de7f304033185102f93e89ff
koushik <koushik.dey@live.com>
2022-07-05 22:10:20 +0300

    cnd 4

--- routes/product.js
+++ routes/product.js
@@ -64,12 +64,11 @@ router.get("/find/:id", async (req, res) => {
 router.get("/", async (req, res) => {
   const qNew = req.query.new;
   const qCategory = req.query.category;
-
   try {
     let products;
 
     if (qNew) {
-       products = await Product.find().sort({ createdAt: -1 }).limit(1);
+      products = await Product.find().sort({ createdAt: -1 }).limit(1);
     } else if (qCategory) {
       products = await Product.find({
         categories: {
@@ -80,10 +79,10 @@ router.get("/", async (req, res) => {
       products = await Product.find();
     }
 
+    res.status(200).json(products);
   } catch (err) {
     res.status(500).json(err);
   }
-
 });
 
 
