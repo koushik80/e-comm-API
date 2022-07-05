dd02a998f9e1042acff2ad7cd4be5eadf30460a6
koushik <koushik.dey@live.com>
2022-07-05 22:05:11 +0300

    cnd

--- routes/product.js
+++ routes/product.js
@@ -70,6 +70,8 @@ router.get("/", async (req, res) => {
 
     if (qNew) {
        products = await Product.find().sort({ createdAt: -1 }).limit(1);
+    } else if (qCategory) {
+      products = await Product.find()
     }
 
   } catch {
