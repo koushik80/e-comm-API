ffb927a112a9bdfb719e4730036d45a7ef77625b
koushik <koushik.dey@live.com>
2022-07-05 22:21:43 +0300

    crt2

--- routes/cart.js
+++ routes/cart.js
@@ -7,6 +7,18 @@ const {
 
 const router = require("express").Router();
 
+//CREATE
+
+router.post("/", verifyToken, async (req, res) => {
+  const newCart = new Cart(req.body);
+
+  try {
+    const savedCart = await newCart.save();
+    res.status(200).json(savedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 
