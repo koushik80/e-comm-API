16b7bf520e72a15ef3a8899100610d02e8d423f3
koushik <koushik.dey@live.com>
2022-07-05 20:51:28 +0300

    prd2

--- routes/product.js
+++ routes/product.js
@@ -4,9 +4,17 @@ const Product = require("../models/Product");
 const router = require("express").Router();
 
 //CREATE
-router.post() (req, res) => {
 
-}
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+    const newProduct = new Product(req.body);
+
+    try {
+        
+    } catch (err) {
+
+    }
+
+});
 
 
 
