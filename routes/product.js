a4b852e2d00ed103e62bda0339009e2962cc065d
koushik <koushik.dey@live.com>
2022-07-05 21:41:34 +0300

    prd-upd

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,7 @@
 const Product = require("../models/Product");
 const {
     verifyToken,
+    verifyTokenAndAuthorization,
     verifyTokenAndAdmin
 } = require("./verifyToken");
 
@@ -19,6 +20,22 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+//UPDATE
+
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 
