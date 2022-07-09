0d9e41306c21590f6fbacdd330305df621badc48
koushik <koushik.dey@live.com>
2022-07-10 01:22:18 +0300

    gusr

--- routes/order.js
+++ routes/order.js
@@ -48,7 +48,16 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+//GET USER ORDERS
 
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const orders = await Order.find({ userId: req.params.userId });
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 module.exports = router;
