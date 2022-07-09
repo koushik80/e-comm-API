26606514d583f355f4a879d45c5149a165dcfc2c
koushik <koushik.dey@live.com>
2022-07-10 01:19:27 +0300

    cr-up

--- routes/order.js
+++ routes/order.js
@@ -20,4 +20,26 @@ router.post("/", verifyToken, async (req, res) => {
   }
 });
 
+//UPDATE
+
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+
+
+
 module.exports = router;
